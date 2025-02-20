8e639954e3370fa31ab478f2a2a9b74f9fe61da9
John <johnprobussr@gmail.com>
2025-02-20 13:28:48 -0600

    add clock project

---
+++ DigitalClock/app.js
@@ -0,0 +1,17 @@
+const digitalClock = () => {
+  const date = new Date();
+
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+  const day = date.getDay();
+
+  const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thurs', 'Fri', 'Sat'];
+
+  const clock = `${weekDays[day]} ${hours}:${minutes}:${seconds}`;
+
+  document.getElementById('clock').innerHTML = clock;
+};
+
+digitalClock();
+setInterval(digitalClock, 1000);
--- salaryTrends/main.js
+++
@@ -1,1 +0,0 @@
-import {getRoles, getCompanies} from './salaryData.js';
