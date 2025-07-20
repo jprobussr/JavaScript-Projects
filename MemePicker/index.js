30269600ceb24d7929c450f4aadc56473f591ec0
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2025-07-20 09:23:42 -0500

    Update Bored-Bot

--- MemePicker/index.js
+++ MemePicker/index.js
@@ -2,14 +2,29 @@ import { catsData } from './data.js';
 
 const emotionRadios = document.getElementById('emotion-radios');
 const getImageBtn = document.getElementById('get-image-btn');
+const gifsOnlyOption = document.getElementById('gifs-only-option');
 
 const getMatchingCatsArray = () => {
+  const selectedEmotion = document.querySelector(
+    'input[type="radio"]:checked'
+  ).value;
+  const isGif = gifsOnlyOption.checked;
+
+  const matchingCatsArray = catsData.filter((cat) => {
+    if (isGif) {
+      return cat.emotionTags.includes(selectedEmotion) && cat.isGif;
+    } else {
+      return cat.emotionTags.includes(selectedEmotion);
+    }
+  });
+
   if (document.querySelector('input[type="radio"]:checked')) {
     const radioInputs = document.querySelector(
       'input[type="radio"]:checked'
     ).value;
-    console.log(radioInputs);
+    
   }
+  return matchingCatsArray
 };
 
 getImageBtn.addEventListener('click', getMatchingCatsArray);
