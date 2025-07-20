ecee929f4374da5a12ed557675a59376b2a881f0
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2025-07-20 12:02:50 -0500

    Add JS to Bored Bot

--- BordedBot/index.js
+++ BordedBot/index.js
@@ -1,18 +1,10 @@
-// document.getElementById('bored-bot').addEventListener('click', getIdea);
-
-// const getIdea = () => {
-//   fetch('https://www.boredapi.com/api/activity').then((res) => {
-//     document.body.classList('fun');
-//     document.getElementById('idea').textContent = data.activity;
-//     document.getElementById('title').textContent = '🦾 HappyBot🦿';
-//   });
-// };
-
-fetch('https://apis.scrimba.com/bored/api/activity')
-.then(response => response.json())
-.then(data => {
- document.getElementById('bored-idea').textContent = data.activity;
-})
-.catch(error => {
-    console.error('Error Message:', error);
+document.getElementById('bored-bot').addEventListener('click', () => {
+  fetch('https://apis.scrimba.com/bored/api/activity')
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById('bored-idea').textContent = data.activity;
+    })
+    .catch((error) => {
+      console.error('Error Message:', error);
+    });
 });
