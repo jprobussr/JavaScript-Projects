a0b70803374dcf5e966a0ef1d7d4246cdb3f4d95
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2025-07-21 12:50:59 -0500

    Complete Bored-Bot Project with Bob Z

--- BordedBot/index.js
+++ BordedBot/index.js
@@ -3,8 +3,20 @@ document.getElementById('bored-bot').addEventListener('click', () => {
     .then((response) => response.json())
     .then((data) => {
       document.getElementById('bored-idea').textContent = data.activity;
+
+      const titles = [
+        '🕹️ FunFinder 1985 👾',
+        '👾 Boredom Blaster 8-Bit 💥',
+        '🎮 Insert Fun Coin 🪙',
+        '🚀 Pixel Party Engine 🧬',
+      ];
+
+      const randomTitle = titles[Math.floor(Math.random() * titles.length)];
+      document.getElementById('title').textContent = randomTitle;
+
+      document.body.classList.add('fun1');
     })
     .catch((error) => {
-      console.error('Error Message:', error);
+      console.error('Network Fetching Error:', error);
     });
 });
